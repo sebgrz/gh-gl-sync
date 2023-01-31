fe65bd327e7480bad76d994a1284cae04772bbea
Seba G <sebag.git@gmail.com>
2023-01-31 21:45:50 +0100

    add git2 library + show log of example repo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::args;
+
+use git2::{Repository, Sort, Error};
+
+fn main() -> Result<(), Error> {
+    let repo_url = args().nth(1).expect("git repository url is not provided");
+    let repo = Repository::clone(repo_url.as_str(), "test")?;
+    let mut revwalk = repo.revwalk()?;
+      
+    revwalk.set_sorting(Sort::NONE)?;
+    revwalk.push_head()?;
+
+    for oid in revwalk {
+        let commit = repo.find_commit(oid?).unwrap();
+        println!("{} - {}", commit.id().to_string(), commit.summary().unwrap_or_else(|| "")) 
+    }
+
+    Ok(())
 }
